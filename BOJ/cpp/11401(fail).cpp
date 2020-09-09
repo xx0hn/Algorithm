03fa1ed3cb1b76bb0d5d097f60e2d90b9f8c2aee
xx0hn <64733814+xx0hn@users.noreply.github.com>
2020-09-09 18:05:28 +0900

    Create 11401(fail).cpp

---
+++ BOJ/cpp/11401(fail).cpp
@@ -0,0 +1,67 @@
+//
+//  main.cpp
+//  practice.cpp
+//
+//  Created by 황승환 on 2020/05/06.
+//  Copyright © 2020 황승환. All rights reserved.
+//
+
+/*
+ 문제
+ 자연수N과 정수K가 주어졌을 때 이항 계수(KN)를 1,000,000,007로 나눈 나머지를 구하는 프로그램을 작성하시오.
+
+ 입력
+ 첫째 줄에 N과 K가 주어진다. (1 ≤ N ≤ 4,000,000, 0 ≤ K ≤ N)
+
+ 출력
+ (KN)를 1,000,000,007로 나눈 나머지를 출력한다.
+ */
+#include <iostream>
+#define endl "\n"
+typedef long long ll;
+const int MOD=1000000007;
+using namespace std;
+
+int n, k;
+ll Inversion(){
+    ll ret=1LL;
+    ll temp=1LL;
+    ll want=MOD-2;
+    for(int i=2; i<=k; i++){
+        temp*=i;
+        temp%=MOD;
+    }
+    while(want){
+        if(want%2){
+            ret*=temp;
+            ret%=MOD;
+        }
+        temp*=temp;
+        temp%=MOD;
+        want/=2;
+    }
+    return ret%MOD;
+}
+
+ll Combi(){
+    ll ret=1LL;
+    for(int i=n; i>n-k; i--){
+        ret*=i;
+        ret%=MOD;
+    }
+    return ret%MOD;
+}
+
+void Solution(){
+    cin>>n>>k;
+    ll a=Inversion();
+    ll b=Combi();
+    cout<<(a*b)%MOD<<endl;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    Solution();
+    return 0;
+}
