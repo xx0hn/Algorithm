6dcdbe641af045613bd8a1728a6bef83dc8fc905
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-07-18 22:31:09 +0900

    Update 15663(fail).cpp

--- BOJ/cpp/15663(fail).cpp
+++ BOJ/cpp/15663(fail).cpp
@@ -13,61 +13,6 @@
 
  수열은 사전 순으로 증가하는 순서로 출력해야 한다.
  */
-/*
-#include <iostream>
-#include <algorithm>
-#define MAX 9
-using namespace std;
-
-int n, m;
-int arr[MAX];
-int result[MAX];
-bool chk[MAX];
-
-void Input(){
-    cin>>n>>m;
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
-    }
-    sort(arr, arr+n);
-}
-
-void Init(){
-    for(int i=0; i<n; i++){
-        chk[i]=false;
-    }
-}
-
-void DFS(int idx){
-    if(idx==m){
-        for(int i=0; i<m; i++){
-            cout<<result[i]<<" ";
-        }
-        cout<<endl;
-        return ;
-    }
-    Init();
-    int xdx=0;
-    for(int i=0; i<n; i++){
-        if(!chk[i]&&arr[i]!=xdx){
-            result[idx]=arr[i];
-            xdx=result[idx];
-            chk[i]=true;
-            DFS(idx+1);
-        }
-    }
-}
-    
-
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    Input();
-    DFS(0);
-    return 0;
-}
-*/
 #include <iostream>
 #include <algorithm>
 #define MAX 8
