31fa73c1de6adb5c428c7cf77a5bb64aa8a0faf9
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-07-22 17:28:40 +0900

    Create 11724.cpp

---
+++ BOJ/cpp/11724.cpp
@@ -0,0 +1,57 @@
+/*
+ 문제
+ 방향 없는 그래프가 주어졌을 때, 연결 요소 (Connected Component)의 개수를 구하는 프로그램을 작성하시오.
+
+ 입력
+ 첫째 줄에 정점의 개수 N과 간선의 개수 M이 주어진다. (1 ≤ N ≤ 1,000, 0 ≤ M ≤ N×(N-1)/2) 둘째 줄부터 M개의 줄에 간선의 양 끝점 u와 v가 주어진다. (1 ≤ u, v ≤ N, u ≠ v) 같은 간선은 한 번만 주어진다.
+
+ 출력
+ 첫째 줄에 연결 요소의 개수를 출력한다.
+ */
+#include <iostream>
+#include <vector>
+#define MAX 1001
+using namespace std;
+
+int n, m;
+vector<int> arr[MAX];
+int cnt=0;
+bool visited[MAX];
+
+void Input(){
+    cin>>n>>m;
+    for(int i=0; i<m; i++){
+        int u,v;
+        cin>>u>>v;
+        arr[u].push_back(v);
+        arr[v].push_back(u);
+    }
+}
+
+void DFS(int start){
+    visited[start]=true;
+    for(int i=0; i<arr[start].size(); i++){
+        int next=arr[start][i];
+        if(!visited[next])
+            DFS(next);
+    }
+}
+
+void Solution(){
+    for(int i=1; i<=n; i++){
+        if(!visited[i]){
+            DFS(i);
+            cnt++;
+        }
+    }
+    cout<<cnt<<endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    Input();
+    Solution();
+    return 0;
+}
