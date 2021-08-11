25ecb917e255ca19a04101ab9b6fcff2f547d0e4
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-08-11 21:48:10 +0900

    Create 11718.cpp

---
+++ BOJ/cpp/11718.cpp
@@ -0,0 +1,33 @@
+/*
+ 문제
+ 입력 받은 대로 출력하는 프로그램을 작성하시오.
+
+ 입력
+ 입력이 주어진다. 입력은 최대 100줄로 이루어져 있고, 알파벳 소문자, 대문자, 공백, 숫자로만 이루어져 있다. 각 줄은 100글자를 넘지 않으며, 빈 줄은 주어지지 않는다. 또, 각 줄은 공백으로 시작하지 않고, 공백으로 끝나지 않는다.
+
+ 출력
+ 입력받은 그대로 출력한다.
+
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ipt;
+
+void Solution(){
+    while(1){
+        getline(cin, ipt);
+        if(ipt=="")
+            break;
+        cout<<ipt<<endl;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    Solution();
+    return 0;
+}
