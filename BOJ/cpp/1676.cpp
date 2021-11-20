f837e5cbc0d560d52622eb2cc6b899e9c6a7926d
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-11-20 16:45:05 +0900

    Update 1676.cpp

--- BOJ/cpp/1676.cpp
+++ BOJ/cpp/1676.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  practice.cpp
-//
-//  Created by 황승환 on 2020/05/06.
-//  Copyright © 2020 황승환. All rights reserved.
-//
-
 /*
  문제
  N!에서 뒤에서부터 처음 0이 아닌 숫자가 나올 때까지 0의 개수를 구하는 프로그램을 작성하시오.
