90109566de527109b47da97827cac891c193372e
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-01-07 00:07:15 +0900

    Create 937.py

---
+++ LeetCode/937.py
@@ -0,0 +1,46 @@
+# 937. Reorder Data in Log Files
+# Easy
+
+# 1487
+
+# 3538
+
+# Add to List
+
+# Share
+# You are given an array of logs. Each log is a space-delimited string of words, where the first word is the identifier.
+
+# There are two types of logs:
+
+# Letter-logs: All words (except the identifier) consist of lowercase English letters.
+# Digit-logs: All words (except the identifier) consist of digits.
+# Reorder these logs so that:
+
+# The letter-logs come before all digit-logs.
+# The letter-logs are sorted lexicographically by their contents. If their contents are the same, then sort them lexicographically by their identifiers.
+# The digit-logs maintain their relative ordering.
+# Return the final order of the logs.
+
+ 
+
+# Example 1:
+
+# Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+# Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+# Explanation:
+# The letter-log contents are all different, so their ordering is "art can", "art zero", "own kit dig".
+# The digit-logs have a relative order of "dig1 8 1 5 1", "dig2 3 6".
+# Example 2:
+
+# Input: logs = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
+# Output: ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters, digits = [], []
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        return letters+digits
