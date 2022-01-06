b7c6629f4e9e2dc0093862349a6077c59beb17fb
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-01-06 23:46:29 +0900

    Create 344(1).py

---
+++ LeetCode/344(1).py
@@ -0,0 +1,37 @@
+# 344. Reverse String
+# Easy
+
+# 3613
+
+# 872
+
+# Add to List
+
+# Share
+# Write a function that reverses a string. The input string is given as an array of characters s.
+
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+# Example 1:
+
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+# Example 2:
+
+# Input: s = ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 105
+# s[i] is a printable ascii character.
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        left, right = 0, len(s)-1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left+=1
+            right-=1
