afa50784ed6b8296163d51f84e5579c436d24236
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-01-14 22:41:42 +0900

    Create 42(2).py

---
+++ LeetCode/42(2).py
@@ -0,0 +1,45 @@
+# 42. Trapping Rain Water
+# Hard
+
+# 16027
+
+# 227
+
+# Add to List
+
+# Share
+# Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+ 
+
+# Example 1:
+
+
+# Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# Output: 6
+# Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+# Example 2:
+
+# Input: height = [4,2,0,3,2,5]
+# Output: 9
+ 
+
+# Constraints:
+
+# n == height.length
+# 1 <= n <= 2 * 104
+# 0 <= height[i] <= 105
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        stack = []
+        volume = 0
+        for i in range(len(height)):
+            while stack and height[i]>height[stack[-1]]:
+                top = stack.pop()
+                if not len(stack):
+                    break
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]])-height[top]
+                volume += distance*waters
+            stack.append(i)
+        return volume
