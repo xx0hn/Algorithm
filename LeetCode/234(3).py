ac029fcf8cba9c06609330b13323c72623947aa0
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-01-15 20:16:19 +0900

    Create 234(3).py

---
+++ LeetCode/234(3).py
@@ -0,0 +1,47 @@
+# 234. Palindrome Linked List
+# Easy
+
+# 7541
+
+# 495
+
+# Add to List
+
+# Share
+# Given the head of a singly linked list, return true if it is a palindrome.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [1,2,2,1]
+# Output: true
+# Example 2:
+
+
+# Input: head = [1,2]
+# Output: false
+ 
+
+# Constraints:
+
+# The number of nodes in the list is in the range [1, 105].
+# 0 <= Node.val <= 9
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        rev = None
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
+        return not rev
