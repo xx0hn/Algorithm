73ce5030355280eb3a082b489357045234ef5d80
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-01-25 01:02:04 +0900

    Create 23.py

---
+++ LeetCode/23.py
@@ -0,0 +1,62 @@
+# 23. Merge k Sorted Lists
+# Hard
+
+# 10064
+
+# 418
+
+# Add to List
+
+# Share
+# You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+# Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+# Example 1:
+
+# Input: lists = [[1,4,5],[1,3,4],[2,6]]
+# Output: [1,1,2,3,4,4,5,6]
+# Explanation: The linked-lists are:
+# [
+#   1->4->5,
+#   1->3->4,
+#   2->6
+# ]
+# merging them into one sorted list:
+# 1->1->2->3->4->4->5->6
+# Example 2:
+
+# Input: lists = []
+# Output: []
+# Example 3:
+
+# Input: lists = [[]]
+# Output: []
+ 
+
+# Constraints:
+
+# k == lists.length
+# 0 <= k <= 10^4
+# 0 <= lists[i].length <= 500
+# -10^4 <= lists[i][j] <= 10^4
+# lists[i] is sorted in ascending order.
+# The sum of lists[i].length won't exceed 10^4.
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        root = result = ListNode(None)
+        heap = []
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i, lists[i]))
+        while heap:
+            node = heapq.heappop(heap)
+            idx = node[1]
+            result.next = node[2]
+            
+            result = result.next
+            if result.next:
+                heapq.heappush(heap, (result.next.val, idx, result.next))
+        return root.next
