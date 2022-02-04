2bd4c15c9273859bbefa3499cac87aa25f5c8751
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-02-04 18:04:19 +0900

    Create 78(2).py

---
+++ LeetCode/78(2).py
@@ -0,0 +1,40 @@
+# 78. Subsets
+# Medium
+
+# 8355
+
+# 133
+
+# Add to List
+
+# Share
+# Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+# The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3]
+# Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+# Example 2:
+
+# Input: nums = [0]
+# Output: [[],[0]]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 10
+# -10 <= nums[i] <= 10
+# All the numbers of nums are unique.
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        results=[]
+        def dfs(index, path):
+            results.append(path)
+            for i in range(index, len(nums)):
+                dfs(i+1, path+[nums[i]])
+        dfs(0, [])
+        return results
