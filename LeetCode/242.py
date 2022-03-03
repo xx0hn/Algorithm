25640d7ec7d9d0c77e27a250f5296b8f62847177
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-03-03 20:38:12 +0900

    Create 242.py

---
+++ LeetCode/242.py
@@ -0,0 +1,33 @@
+# 242. Valid Anagram
+# Easy
+
+# 4305
+
+# 202
+
+# Add to List
+
+# Share
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+# Example 2:
+
+# Input: s = "rat", t = "car"
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s)==sorted(t)
