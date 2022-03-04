037cbf8c69324901f6b93f4da17edbe55949f7e8
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-03-04 21:22:54 +0900

    Create 33.py

---
+++ LeetCode/33.py
@@ -0,0 +1,64 @@
+# 33. Search in Rotated Sorted Array
+# Medium
+
+# 12889
+
+# 849
+
+# Add to List
+
+# Share
+# There is an integer array nums sorted in ascending order (with distinct values).
+
+# Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+# Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+# Example 1:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# Example 2:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+# Example 3:
+
+# Input: nums = [1], target = 0
+# Output: -1
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 5000
+# -104 <= nums[i] <= 104
+# All values of nums are unique.
+# nums is an ascending array that is possibly rotated.
+# -104 <= target <= 104
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        left, right=0, len(nums)-1
+        while left<right:
+            mid=left+(right-left)//2
+            if nums[mid]>nums[right]:
+                left=mid+1
+            else:
+                right=mid
+        pivot = left
+        left, right=0, len(nums)-1
+        while left<=right:
+            mid=left+(right-left)//2
+            mid_pivot=(mid+pivot)%len(nums)
+            if nums[mid_pivot]<target:
+                left=mid+1
+            elif nums[mid_pivot]>target:
+                right=mid-1
+            else:
+                return mid_pivot
+        return -1
