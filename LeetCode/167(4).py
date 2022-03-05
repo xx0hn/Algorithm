e7de0b79890d379bac82c497c3c15171ea6d5b64
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-03-05 23:06:57 +0900

    Update 167(4).py

--- LeetCode/167(4).py
+++ LeetCode/167(4).py
@@ -45,7 +45,6 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         for k, v in enumerate(numbers):
-            left, right=k+1, len(numbers)-1
             expected=target-v
             i=bisect.bisect_left(numbers[k+1:], expected)
             if i<len(numbers[k+1:]) and numbers[i+k+1]==expected:
