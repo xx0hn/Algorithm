daa94cfe8264e2b009f675516fcc894f4d4c22a3
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-03-14 23:37:49 +0900

    Create 239.py

---
+++ LeetCode/239.py
@@ -0,0 +1,57 @@
+# 239. Sliding Window Maximum
+# Hard
+
+# 9120
+
+# 318
+
+# Add to List
+
+# Share
+# You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+# Return the max sliding window.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+# Output: [3,3,5,5,6,7]
+# Explanation: 
+# Window position                Max
+# ---------------               -----
+# [1  3  -1] -3  5  3  6  7       3
+#  1 [3  -1  -3] 5  3  6  7       3
+#  1  3 [-1  -3  5] 3  6  7       5
+#  1  3  -1 [-3  5  3] 6  7       5
+#  1  3  -1  -3 [5  3  6] 7       6
+#  1  3  -1  -3  5 [3  6  7]      7
+# Example 2:
+
+# Input: nums = [1], k = 1
+# Output: [1]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -104 <= nums[i] <= 104
+# 1 <= k <= nums.length
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        results = []
+        window = collections.deque()
+        current_max = float('-inf')
+        for i, v in enumerate(nums):
+            window.append(v)
+            if i < k-1:
+                continue
+            if current_max == float('-inf'):
+                current_max = max(window)
+            elif v > current_max:
+                current_max = v
+            results.append(current_max)
+            if current_max == window.popleft():
+                current_max = float('-inf')
+        return results
