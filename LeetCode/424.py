5216c538257f11af4e5ed8eb7c57630c2368bfad
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-03-15 21:47:40 +0900

    Create 424.py

---
+++ LeetCode/424.py
@@ -0,0 +1,45 @@
+# 424. Longest Repeating Character Replacement
+# Medium
+
+# 4009
+
+# 158
+
+# Add to List
+
+# Share
+# You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+# Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+ 
+
+# Example 1:
+
+# Input: s = "ABAB", k = 2
+# Output: 4
+# Explanation: Replace the two 'A's with two 'B's or vice versa.
+# Example 2:
+
+# Input: s = "AABABBA", k = 1
+# Output: 4
+# Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+# The substring "BBBB" has the longest repeating letters, which is 4.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 105
+# s consists of only uppercase English letters.
+# 0 <= k <= s.length
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = right = 0
+        counts = collections.Counter()
+        for right in range(1, len(s) + 1):
+            counts[s[right - 1]] += 1
+            max_char_n = counts.most_common(1)[0][1]
+            if right - left - max_char_n > k:
+                counts[s[left]] -= 1
+                left += 1
+        return right - left
