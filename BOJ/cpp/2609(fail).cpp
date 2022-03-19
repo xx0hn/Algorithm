8050b506e98ac6558820c6ce2cea8e33d30335f8
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-03-19 10:25:32 +0900

    Update 2609(fail).cpp

--- BOJ/cpp/2609(fail).cpp
+++ BOJ/cpp/2609(fail).cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  practice.cpp
-//
-//  Created by 황승환 on 2020/05/06.
-//  Copyright © 2020 황승환. All rights reserved.
-//
-
 /*
  문제
  두 개의 자연수를 입력받아 최대 공약수와 최소 공배수를 출력하는 프로그램을 작성하시오.
