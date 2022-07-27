6c1e03cd8d2c2fc672ce0e75b0ce2f0b07fc1ee3
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-07-27 15:39:21 +0900

    Create 9252.py

---
+++ BOJ/python/9252.py
@@ -0,0 +1,26 @@
+# 문제
+# LCS(Longest Common Subsequence, 최장 공통 부분 수열)문제는 두 수열이 주어졌을 때, 모두의 부분 수열이 되는 수열 중 가장 긴 것을 찾는 문제이다.
+#
+# 예를 들어, ACAYKP와 CAPCAK의 LCS는 ACAK가 된다.
+#
+# 입력
+# 첫째 줄과 둘째 줄에 두 문자열이 주어진다. 문자열은 알파벳 대문자로만 이루어져 있으며, 최대 1000글자로 이루어져 있다.
+#
+# 출력
+# 첫째 줄에 입력으로 주어진 두 문자열의 LCS의 길이를, 둘째 줄에 LCS를 출력한다.
+#
+# LCS가 여러 가지인 경우에는 아무거나 출력하고, LCS의 길이가 0인 경우에는 둘째 줄을 출력하지 않는다.
+a = [''] + list(str(input()))
+b = [''] + list(str(input()))
+dp = [['' for _ in range(len(b))] for _ in range(len(a))]
+for i in range(1, len(a)):
+    for j in range(1, len(b)):
+        if a[i] == b[j]:
+            dp[i][j] = dp[i-1][j-1]+a[i]
+        else:
+            if len(dp[i-1][j]) > len(dp[i][j-1]):
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i][j-1]
+print(len(dp[len(a)-1][len(b)-1]))
+print(dp[len(a)-1][len(b)-1])
